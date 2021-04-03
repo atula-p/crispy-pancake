1cdd60969f47076a5d54415c24df1d79f8dbe4cd
Atula Poduval <atula.poduval@gmail.com>
2021-04-03 20:29:11 +0200

    bootstrap template added

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+#@app.route('/')
+#def hello():
+#    return 'yoohoo!'
+
+
 @app.route('/')
-def hello():
-    return 'yoohoo!'
-
-
-@app.route('/index')
 def cal():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
