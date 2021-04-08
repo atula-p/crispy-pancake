659a6821c1c0c85a29feea8ac956d98581af1eba
Bartulovic <marko.bartulovic1@gmail.com>
2021-04-08 19:42:44 +0200

    added languages transl.

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
 app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return 'de'
+    #request.accept_languages.best_match(['en', 'de'])
 
 #@app.route('/')
 #def hello():
 #    return 'yoohoo!'
 
+
 @app.route('/')
-def cal():
-    return render_template('index.html')
+def index():
+    driving_school = gettext('Driving School')
+    greetings = gettext('Hi, how are you?')
+    answer = gettext('I am fine')
+    return render_template('index.html', driving_school=driving_school, greetings=greetings, answer=answer)
 
 
 @app.route('/about')
@@ -28,6 +42,7 @@ def contact():
 @app.route('/faq')
 def faq():
     return render_template('faq.html')
+
 
 @app.route('/layout')
 def layout():
